06aaeb76a2ad2337aa24dd3bc65982dad542be5f
jet2018 <ezrajet9@gmail.com>
2021-03-29 12:16:56 +0300

    made a small change

--- sections/faqs.php
+++ sections/faqs.php
@@ -9,7 +9,7 @@
                 <ul>
                     <li data-aos="fade-up" data-aos-delay="100">
                         <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse"
-                            data-bs-target="#faq-list-1">Non consectetur a erat nam at lectus urna duis? <i
+                            data-bs-target="#faq-list-1">How do I join the club<i
                                 class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
                         <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
                             <p>
